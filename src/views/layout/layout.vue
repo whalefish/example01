b62f91495760dcdbaaacfba1cc5a114d2857a749
張珈語 <chiayu.chang@udngroup.com.tw>
2024-09-01 15:49:41 +0800

    實作 layout

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -3,12 +3,38 @@
         <div class="topbar">
             
         </div>
-    </div>
-    <div class="main-content">
-        
+        <div class="main-content">
+            <router-view>
+                <component :is="Component" :key="$route.name"></component>
+            </router-view>
+            <div class="footer">
+                Fish 著作權所有 © 2024
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
-
 </script>
 <style lang="scss" scoped>
+.layout {
+    height: 100vh;
+    position: relative;
+}
+
+.main-content {
+    height: calc(100vh - $topbar-hasPicture-height);
+    display: flex;
+    flex-direction: column;
+    .view {
+        flex-grow: 1;
+    }
+
+    .footer {
+        text-align: center;
+        padding: 16px 0;
+        font-size: $font-size-12;
+        color: $white;
+        background-color: $black;
+    }
+}
+</style>
